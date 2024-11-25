7586ad80de878862c082cba2ffd0a25a68870790
Milagros Cornidez <mcornidez@itba.edu.ar>
2024-11-25 11:28:33 -0300

    change logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Image from "next/image";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -19,17 +20,30 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+      <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <header className="fixed top-0 left-0 w-full bg-white shadow-md z-50">
+        <div className="container mx-auto flex items-center justify-between py-4 px-4">
+          <Image
+              src="/images/logo-ln.webp"
+              alt="Logo La Nación"
+              width={150}
+              height={50}
+              priority
+          />
+        </div>
+      </header>
+      <main className="pt-24">
         {children}
+      </main>
       </body>
-    </html>
+      </html>
   );
 }
