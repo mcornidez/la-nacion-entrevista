3ccc48e8d80e5f47d0e9a63c0b90b2d7c68cf2d8
Milagros Cornidez <mcornidez@itba.edu.ar>
2024-11-25 11:31:41 -0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import Image from "next/image";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -17,6 +16,9 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "La Nación",
   description: "Últimas noticias de Argentina y el mundo",
+  icons: {
+    icon: '/images/logo-ln.webp'
+  }
 };
 
 export default function RootLayout({
@@ -29,20 +31,7 @@ export default function RootLayout({
       <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      <header className="fixed top-0 left-0 w-full bg-white shadow-md z-50">
-        <div className="container mx-auto flex items-center justify-between py-4 px-4">
-          <Image
-              src="/images/logo-ln.webp"
-              alt="Logo La Nación"
-              width={150}
-              height={50}
-              priority
-          />
-        </div>
-      </header>
-      <main className="pt-24">
-        {children}
-      </main>
+      {children}
       </body>
       </html>
   );
